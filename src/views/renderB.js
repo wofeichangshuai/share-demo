7337e46b966d62fa4daadb89f770d28313fc1d7e
wofeichangshuai <1536188896@qq.com>
2021-07-05 23:29:06 +0800

    update

--- src/views/renderB.js
+++ src/views/renderB.js
@@ -1,11 +1,17 @@
 export default {
   name: "RenderB",
   render: function(h) {
+    var domFun = function() {
+      return {
+        template: `<div>谈笑风生</div>`,
+      };
+    };
+    return h(domFun());
     // const fun = function() {
     //   return { template: `<div>啦啦啦啦啦啦啦</div>` };
     // };
     // return h(fun());
-    return h({ template: "<div>啦啦啦啦啦啦啦</div>" });
+    // return h({ template: "<div>啦啦啦啦啦啦啦</div>" });
     // return h("div", "renderB");
   },
 };
